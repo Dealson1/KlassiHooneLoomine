0c7289bced42100839c714cdf70d9959508e2726
alexey <alexborn950@gmail.com>
2022-01-24 16:40:51 +0200

    done

--- Program.cs
+++ Program.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace KlassiHooneLoomine
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            EraMaja majake = new EraMaja();
+            Inimene mees = new Inimene("Juku");
+            majake.uks = new Uks("must");
+            mees.hoone = majake;
+            mees.NaitaInfo();
+            
+            Console.ReadLine();
         }
     }
 }
